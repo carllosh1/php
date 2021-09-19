346bdc58fea6960861d5ce567ac30175befe868e
Carlos Henrique Oliveira Santos <69221726+carllosh1@users.noreply.github.com>
2021-09-18 23:33:09 -0300

    Delete aula010 directory

--- aula010/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-    try {
-        $con= new PDO("mysql:host=localhost;dbname=db_sa02_dsw","root","");   
-    } catch (PDOException $e) {
-        echo "Erro no banco: " . $e->getMessage();
-    }
-?>
